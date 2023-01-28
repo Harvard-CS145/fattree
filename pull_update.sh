30fac2b9201ce66672b7ee44c5a8649fc5990b40
Vic Feng <wfeng@g.harvard.edu>
2023-01-28 13:29:18 -0500

    Make it more robust

--- pull_update.sh
+++ pull_update.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
 
-git remote add upstream "https://github.com/Harvard-CS145/cs145-23-project1.git"
+res=$(git remote -v | grep upstream)
+
+if [ -z "$res" ]
+then
+    git remote add upstream "https://github.com/Harvard-CS145/cs145-23-project1.git"
+fi
+
 git pull upstream main --allow-unrelated-histories
