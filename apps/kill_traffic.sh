1e7a7ecac51415c7b1831f67e2a562adb8547e3d
laochonlam <laochanlam@gmail.com>
2024-01-14 05:12:48 -0500

    remove static apps directory

--- apps/kill_traffic.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-# Usage ./apps/kill_traffic.sh
-#   Kill the traffic generator background processes
-
-sudo killall -9 "python apps/memcached_client.py" 2>/dev/null
-sudo killall -9 memcached 2>/dev/null
-sudo killall -9 "traffic_sender" 2>/dev/null
-sudo killall -9 "traffic_receiver" 2>/dev/null
